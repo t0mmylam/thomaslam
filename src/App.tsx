e698dd8ebb9ae97d2423eba5f1f73980bf09c941
Tommy Lam <tommylam1004@gmail.com>
2024-01-22 14:06:56 -0500

    /projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Tabs, Tab } from "@nextui-org/react";
+import { useLocation } from "react-router-dom";
 import useDarkMode from "use-dark-mode";
 import ThemeSwitcher from "./components/ThemeSwitcher";
 import Home from "./components/Home";
@@ -7,6 +8,15 @@ import ProjectsTab from "./components/ProjectsTab";
 
 function App() {
     const darkMode = useDarkMode();
+    const location = useLocation();
+
+    const getSelectedKey = () => {
+        const path = location.pathname;
+        if (path.includes("/projects")) {
+            return "projects";
+        }
+        return "home";
+    };
 
     return (
         <main
@@ -16,7 +26,7 @@ function App() {
         >
             <div className="relative flex flex-col max-w-sm md:max-w-3xl m-auto p-8">
                 <div className="flex flex-col w-full">
-                    <Tabs aria-label="Pages">
+                    <Tabs defaultSelectedKey={getSelectedKey()} aria-label="Pages">
                         <Tab key="home" title="Home" className="w-full">
                             <Home />
                         </Tab>
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { NextUIProvider } from "@nextui-org/react";
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <NextUIProvider className="h-full w-full">
+            <Router>
             <App />
+            </Router>
         </NextUIProvider>
     </React.StrictMode>
 );
